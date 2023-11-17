0e7f8fd5fc6da5e8e05a8e13c338905be80132c0
VladyslavKshenin <134953305+VladyslavKshenin@users.noreply.github.com>
2023-11-17 18:43:34 +0000

    Deploying to gh-pages from @ VladyslavKshenin/Test_RTFM_project_team_JS@8f738a846d57722ef5ae88eec8ca736ded6ed6f2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,50 +1,43 @@
-import{a as i,b as f,c as d,d as g,l as h}from"./assets/applebooks@2x-9bf15c24.js";const m="/Test_RTFM_project_team_JS/assets/books-desk@1x-1f62c9bc.png",u="/Test_RTFM_project_team_JS/assets/books-desk@2x-306b4264.png",b="/Test_RTFM_project_team_JS/assets/books-mob@1x-93df6442.png",k=[{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"},{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"},{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"}];localStorage.setItem("localCard",JSON.stringify(k));const r="localCard",o=document.querySelector(".js-shopping-list"),n=document.querySelector(".shopping-list-container"),a=p(r);a[0]!==void 0?(n.innerHTML="",o.innerHTML="",o.insertAdjacentHTML("afterbegin",l(a)),o.addEventListener("click",v)):c();function v(s){if(s.target.nodeName==="BUTTON"||s.target.nodeName==="svg"||s.target.nodeName==="use")o.innerHTML="",a.splice(s.target.id,1),localStorage.removeItem(r),y(r,a),a[0]!==void 0?o.insertAdjacentHTML("afterbegin",l(p(r))):c();else return}function l(s){return s.map(({sebel:e},t)=>`<li class="shopping-list-item">
-      <img class="shopping-list-card-img" src="https://storage.googleapis.com/du-prd/books/images/9780670785933.jpg" alt="book">
+import{a as p,b as $,c as d,d as T,l as x}from"./assets/applebooks@2x-32274212.js";const y="/Test_RTFM_project_team_JS/assets/books-desk@1x-1f62c9bc.png",S="/Test_RTFM_project_team_JS/assets/books-desk@2x-306b4264.png",v="/Test_RTFM_project_team_JS/assets/books-mob@1x-93df6442.png",a="shoppingCard",i="shoppingList",o=document.querySelector(".js-shopping-list"),c=document.querySelector(".shopping-list-container"),L="The daughter of a Korean mother and Jewish American father, and leader of the indie rock project Japanese Breakfast, describes creating her own identity after losing her mother to cancer.",n=l(a),g=l(i);n[0]!==void 0?(c.innerHTML="",o.innerHTML="",o.insertAdjacentHTML("afterbegin",m(n)),o.addEventListener("click",M)):f();function M(s){let e=s.target.closest(".btn-shop");if(e)o.innerHTML="",n.splice(e.id,1),g.splice(e.id,1),localStorage.removeItem(a),localStorage.removeItem(i),h(a,n),h(i,g),n[0]!==void 0?o.insertAdjacentHTML("afterbegin",m(l(a))):f();else return}function m(s){return s.map(({book_image:e,title:t,author:b,description:r,amazonLinks:u,appleLinks:k},_)=>(r||(r=L),`<li class="shopping-list-item">
+      <img class="shopping-list-card-img" src="${e}" alt="${t}">
       <div class="shopping-list-card-container">
-        <h2 class="shopping-list-card-title">I will find you</h2>
+        <h2 class="shopping-list-card-title">${t}</h2>
         <p class="shopping-list-card-text">Hardcover fiction</p>
-        <p class="shopping-list-card-desc">
-          David Burroughs was once a devoted father to his three-year-old son Matthew, living a dream life just a short
-          drive away
-          from the working-class suburb where he and his wife, Cheryl, first fell in love--until one fateful night when
-          David woke
-          suddenly to discover Matthew had been murdered while David was asleep just down the hall.
-          ${e}
-        </p>
+        <p class="shopping-list-card-desc">${r}</p>
         <div class="shopping-list-card-bottom-container">
-          <p class="shopping-list-card-author">Harlan Coben</p>
+          <p class="shopping-list-card-author">${b}</p>
           <div class="shopping-list-card-container-img">
-            <a href="" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
-              <img class="shopping-list-card-icon-amazon"srcset="${i} 1x, ${f} 2x" src="${i}" alt="Amazon" loading="lazy">
+            <a href="${u}" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
+              <img class="shopping-list-card-icon-amazon"srcset="${p} 1x, ${$} 2x" src="${p}" alt="Amazon" loading="lazy">
             </a>
-            <a href="" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
-              <img class="shopping-list-card-icon-book" srcset="${d} 1x, ${g} 2x" src="${d}" alt="Apple books" loading="lazy">
+            <a href="${k}" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
+              <img class="shopping-list-card-icon-book" srcset="${d} 1x, ${T} 2x" src="${d}" alt="Apple books" loading="lazy">
             </a>
           </div>
         </div>
         <div>
-          <button class="btn-shop shopping-list-card-container-trash" id="${t}">
+          <button class="btn-shop shopping-list-card-container-trash" id="${_}">
             <svg class="shopping-list-card-icon-trash" width="18px" height="18px">
-              <use href="${h}#icon-del"></use>
+              <use href="${x}#icon-del"></use>
             </svg>
           </button>
         </div>
       </div>
     </li>
-  `).join("")}function c(){n.innerHTML="",n.insertAdjacentHTML("afterbegin",`
+  `)).join("")}function f(){c.innerHTML="",c.insertAdjacentHTML("afterbegin",`
     <p class="shopping-list-text">
       This page is empty, add some books and proceed to order.
     </p>
     <img
       class="shopping-list-image"
       srcset="
-        ${m} 1x,
-        ${u} 2x
+        ${y} 1x,
+        ${S} 2x
       "
-      src="${b}"
+      src="${v}"
       alt="books"
       width="265"
       height="198"
     />
-    `)}function y(s,e){try{const t=JSON.stringify(e);localStorage.setItem(s,t)}catch(t){console.error("Get state error: ",t.message)}}function p(s){try{const e=localStorage.getItem(s);return e===null?void 0:JSON.parse(e)}catch(e){console.error("Get state error: ",e.message)}}
+    `)}function h(s,e){try{const t=JSON.stringify(e);localStorage.setItem(s,t)}catch(t){console.error("Get state error: ",t.message)}}function l(s){try{const e=localStorage.getItem(s);return e===null?void 0:JSON.parse(e)}catch(e){console.error("Get state error: ",e.message)}}
 //# sourceMappingURL=commonHelpers2.js.map
